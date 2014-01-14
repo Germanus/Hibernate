4676346d7ec393756b063a3f9af55aa9fe8de310
Germanus <Germanus@github.com>
2014-01-14 23:29:17 +0300

    старт базы из папки lib

--- runServer.bat
+++ runServer.bat
@@ -1,2 +1,2 @@
 cd data
-@java -classpath c:\Users\User\.m2\repository\org\hsqldb\hsqldb\2.2.9\hsqldb-2.2.9.jar org.hsqldb.server.Server -database.0 file:mydb -dbname.0 hiber
+@java -classpath lib\hsqldb-2.2.9.jar org.hsqldb.server.Server -database.0 file:mydb -dbname.0 hiber
