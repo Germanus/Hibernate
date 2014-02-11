3eff49fd60610415391bd4090c8e6611b68b65f0
ilya.kazakevich <ilya.kazakevich@r-style.com>
2014-02-11 16:49:35 +0300

    test-rebase

--- src/main/java/my/ilya/kind/action/KindDemo.java
+++ src/main/java/my/ilya/kind/action/KindDemo.java
@@ -16,8 +16,7 @@ public final class KindDemo {
         //Пока объект не сохранен он - transient
         Phone phone = new Phone();
         phone.setNumber(5L);
-        //mater flamaster
-
+        //test
 
         Session session = sessionFactory.openSession();
         session.beginTransaction();
