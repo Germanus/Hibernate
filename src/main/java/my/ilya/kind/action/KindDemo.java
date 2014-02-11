8d2d1124152ec34e3fd6bc8631060046295c1462
ilya.kazakevich <ilya.kazakevich@r-style.com>
2014-02-11 17:19:04 +0300

    price

--- src/main/java/my/ilya/kind/action/KindDemo.java
+++ src/main/java/my/ilya/kind/action/KindDemo.java
@@ -18,6 +18,7 @@ public final class KindDemo {
         phone.setNumber(5L);
         //test branch
         //second test
+        //master price
         Session session = sessionFactory.openSession();
         session.beginTransaction();
         session.save(phone);
