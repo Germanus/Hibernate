1085aea330e37c18d19ecac95a3927f29a12da26
ilya.kazakevich <ilya.kazakevich@r-style.com>
2014-02-11 17:08:28 +0300

    master

--- src/main/java/my/ilya/kind/action/KindDemo.java
+++ src/main/java/my/ilya/kind/action/KindDemo.java
@@ -16,8 +16,7 @@ public final class KindDemo {
         //Пока объект не сохранен он - transient
         Phone phone = new Phone();
         phone.setNumber(5L);
-        //test
-        //remote branch
+        //master
 
         Session session = sessionFactory.openSession();
         session.beginTransaction();
