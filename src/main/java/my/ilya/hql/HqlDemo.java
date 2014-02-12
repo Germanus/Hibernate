ae9d9850acda902f5c3216dcc8bb737b84c96213
ilya.kazakevich <ilya.kazakevich@r-style.com>
2014-02-12 10:21:59 +0300

    HQL demo

---
+++ src/main/java/my/ilya/hql/HqlDemo.java
@@ -0,0 +1,32 @@
+package my.ilya.hql;
+
+import my.ilya.common.HibernateUtil;
+import my.ilya.crud.model.Phone;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+import java.util.List;
+
+public final class HqlDemo {
+
+    private static SessionFactory sessionFactory = HibernateUtil.configureSessionFactory();
+    private HqlDemo(){}
+
+    public static void main(String[] args) {
+
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        Query query = session.createQuery("from Phone");
+        List<Phone> list = query.list();
+        //После того как открыта сессия и объект сохранет
+        //до закрытия сессии это persistent объект
+        session.getTransaction().commit();
+        session.close();
+
+        System.out.println("Size:" + list.size());
+
+    }
+
+}
