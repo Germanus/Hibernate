5f2fc8f97b1b5be79741a6deff8144f8600349ab
Germanus <Germanus@github.com>
2014-02-23 00:47:55 +0300

    Projection and Criteria API

--- src/main/java/my/ilya/criteria/CriteriaApiDemo.java
+++ src/main/java/my/ilya/criteria/CriteriaApiDemo.java
@@ -3,11 +3,12 @@ package my.ilya.criteria;
 import my.ilya.common.HibernateUtil;
 import my.ilya.crud.model.Phone;
 
+import my.ilya.dto.Address;
 import org.hibernate.Criteria;
 import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.criterion.Restrictions;
+import org.hibernate.criterion.*;
 
 import java.util.List;
 
@@ -24,6 +25,29 @@ public final class CriteriaApiDemo {
         Criteria criteria = session.createCriteria(Phone.class);
         criteria.add(Restrictions.ge("id",40)).add(Restrictions.between("number", 4L,6L));
         printPhones(criteria.list());
+
+        Criteria criteria2 = session.createCriteria(Phone.class)
+                .setProjection(Projections.max("id"));
+        System.out.println(criteria2.uniqueResult());
+
+
+        Criteria criteria3 = session.createCriteria(Phone.class)
+                .setProjection(Projections.property("id"));
+        System.out.println(criteria3.list());
+
+        Criteria criteria4 = session.createCriteria(Phone.class)
+                .addOrder(Order.desc("number"));
+        printPhones(criteria4.list());
+
+        //По примеру
+        Phone phone = new Phone();
+        phone.setNumber(235L);
+        Example example = Example.create(phone);
+        Criteria criteria5 = session.createCriteria(Phone.class)
+                .add(example);
+        printPhones(criteria5.list());
+
+
 
         session.getTransaction().commit();
         session.close();
